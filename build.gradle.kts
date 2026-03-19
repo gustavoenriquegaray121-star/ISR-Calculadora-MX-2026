f9f4a90a6bc0c55930211c3266d3cbcdb4b4ef4a
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-03-19 05:08:26 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,3 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
