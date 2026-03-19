ce5707c1278214878b62f5d17564b8e51d0d594f
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-03-19 04:12:57 -0600

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "ISR-Calculadora-MX-2026"
+include(":app")
